a9e35351dfbdd5ac4301e1dd022108b4dfb65f04
M.Byun <48914716+bombamong@users.noreply.github.com>
2020-07-29 11:18:22 +0900

    :sparkles: [Client] feature/signout request function (#59)
    - 서버에 로그아웃 요청
    
    Co-authored-by: mw <mwbyun@mws-MacBook-Pro.local>

--- src/screens/Signout.tsx
+++ src/screens/Signout.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import {Dimensions, SafeAreaView, Button, Text, StyleSheet} from 'react-native';
+import Server from '../utils/Server';
 
 interface SignoutProps {}
 
 const Signout: React.FC<SignoutProps> = ({loginProps}) => {
   const {changeLogin} = loginProps;
 
+  const requestSignout: () => void = () => {
+    let url = `http://${Server.server}/user/signout`;
+    let options = {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+    };
+    fetch(url, options).then(() => {
+      if (res.status(200)) {
+        changeLogin(false);
+      } else {
+        throw new Error('bad signout request');
+      }
+    });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Signout</Text>
-      <Button onPress={() => changeLogin(false)} title="signout" />
+      <Button onPress={requestSignout} title="signout" />
     </SafeAreaView>
   );
 };
